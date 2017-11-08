b6f57c4a046612090ac7e4a887027733e49f362d
BlackSabbathX <josepadillainjema01@gmail.com>
2017-11-08 14:15:43 -0500

    Circulos ok

--- src/UselessMainClass.java
+++ src/UselessMainClass.java
@@ -13,7 +13,7 @@ public class UselessMainClass extends PApplet {
     }
 
     public void setup() {
-        frameRate(100);
+        frameRate(200);
         circulos = new Circulos(width / 2, height / 2);
         background(200, 200, 200);
     }
@@ -29,13 +29,13 @@ public class UselessMainClass extends PApplet {
         public Circulos(float x, float y) {
             this.x = 0;
             this.y = 0;
-            this.ancho = 35;
+            this.ancho = 70;
             this.rad = 1;
             Colores();
         }
 
         public void Colores() {
-            int color = (int) random(70, 250);
+            int color = (int) random(0, 20);
             float r, g, b;
             switch (color) {
                 case 0:
@@ -58,50 +58,85 @@ public class UselessMainClass extends PApplet {
                     g = 197;
                     b = 255;
                     break;
-                case 0:
+                case 4:
                     r = 255;
                     g = 153;
                     b = 41;
                     break;
-                case 0:
+                case 5:
                     r = 41;
                     g = 255;
                     b = 189;
                     break;
-                case 0:
+                case 6:
                     r = 255;
                     g = 79;
                     b = 41;
                     break;
-                case 0:
+                case 7:
                     r = 250;
                     g = 170;
                     b = 255;
                     break;
-                case 0:
-                    r = 73
+                case 8:
+                    r = 73;
                     g = 83;
                     b = 118;
                     break;
-                case 0:
+                case 9:
                     r = 220;
                     g = 221;
                     b = 225;
                     break;
-                case 0:
+                case 10:
                     r = 255;
                     g = 27;
                     b = 115;
                     break;
-                case 0:
+                case 11:
                     r = 217;
                     g = 0;
                     b = 84;
                     break;
-                case 0:
-                    r = 41;
-                    g = 255;
-                    b = 189;
+                case 12:
+                    r = 202;
+                    g = 108;
+                    b = 255;
+                    break;
+                case 13:
+                    r = 255;
+                    g = 228;
+                    b = 110;
+                    break;
+                case 14:
+                    r = 124;
+                    g = 169;
+                    b = 255;
+                    break;
+                case 15:
+                    r = 195;
+                    g = 215;
+                    b = 255;
+                    break;
+                case 16:
+                    r = 170;
+                    g = 229;
+                    b = 230;
+                    break;
+                case 17:
+                    r = 56;
+                    g = 188;
+                    b = 190;
+                    break;
+                case 18:
+                    r = 255;
+                    g = 99;
+                    b = 75;
+                    break;
+                case 19:
+                    r = 178;
+                    g = 24;
+                    b = 0;
                     break;
                 default:
                     r = 75;
